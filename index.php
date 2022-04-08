019a411acbfe7dcf8846d212958666fb1fe9c034
j-segura <juanda.segura2005@gmail.com>
2022-04-08 10:34:07 -0500

    header responsive melo caramelo

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./resources/css/styles.css">
-    <link rel="stylesheet" href="./resources/css/responsive1.css">
+    <link rel="stylesheet" href="./resources/css/main.css">
+    <link rel="stylesheet" href="./resources/css/responsive.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </head>
 <body>
@@ -14,7 +14,7 @@
     <div class="container">
 
         <header>
-            <h1>¿Interesado? Cotiza tu sitio aquí​</h1>
+            <h1>Cotiza tu sitio aquí​</h1>
             <h2>La inversión aproximada para el desarrollo de tu sitio web es:</h2>
             <h4 id="total">$ _________</h4>
         </header>
