3d39894af8cde9492d65df19b5aeaa53fea983dc
j-segura <juanda.segura2005@gmail.com>
2022-04-09 11:16:22 -0500

    opciones de las preguntas del formularias centradas responsive

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./resources/css/main.css">
+    <link rel="stylesheet" href="./resources/css/main1.css">
     <link rel="stylesheet" href="./resources/css/responsive.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </head>
@@ -847,6 +847,7 @@
                         
                 </div>
         </div>
+
 
         <div class="step-img"  id="img-progress2">
                 <div class="circulo" id="step1-form2">
